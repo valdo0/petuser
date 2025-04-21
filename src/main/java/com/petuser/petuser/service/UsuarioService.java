487a2a61ee4278dc0584b7051bbef1cc1e8dd403
valdo0 <sebastian.valdivia97gmail.com>
2025-04-20 21:42:55 -0400

    Refactor la interfaz UsuarioService para utilizar objetos de solicitud y respuesta, y agregar métodos para login y actualización de usuarios

--- src/main/java/com/petuser/petuser/service/UsuarioService.java
+++ src/main/java/com/petuser/petuser/service/UsuarioService.java
@@ -2,10 +2,18 @@ package com.petuser.petuser.service;
 
 import java.util.List;
 
-import com.petuser.petuser.model.Usuario;
+import com.petuser.petuser.api.request.UsuarioCreateRequest;
+import com.petuser.petuser.api.request.UsuarioLoginRequest;
+import com.petuser.petuser.api.request.UsuarioPatchRequest;
+import com.petuser.petuser.api.response.UsuarioGetResponse;
 
 public interface UsuarioService {
-    List<Usuario> getAllUsuarios();
-    Usuario getUsuarioById(int id);
-    Usuario addUsuario(Usuario usuario);
+    List<UsuarioGetResponse> getAllUsuarios();
+    UsuarioGetResponse getUsuarioById(Long id);
+    UsuarioGetResponse addUsuario(UsuarioCreateRequest usuario);
+    UsuarioGetResponse login(UsuarioLoginRequest usuarioLoginRequest);
+    UsuarioGetResponse getUsuarioByEmail(String email);
+    boolean deleteUsuario(Long id);
+    UsuarioGetResponse updateUsuario(Long id, UsuarioCreateRequest usuario);
+    UsuarioGetResponse patchUsuario(Long id, UsuarioPatchRequest usuario);
 }
