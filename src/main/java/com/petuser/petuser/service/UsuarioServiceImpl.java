4c3ba46d9a97240a521f3bac6cae00cae3687921
valdo0 <sebastian.valdivia97gmail.com>
2025-04-20 21:42:58 -0400

    Implementa métodos de gestión de usuarios en UsuarioServiceImpl, incluyendo creación, login, actualización y eliminación de usuarios, utilizando UsuarioRepository para persistencia.

--- src/main/java/com/petuser/petuser/service/UsuarioServiceImpl.java
+++ src/main/java/com/petuser/petuser/service/UsuarioServiceImpl.java
@@ -1,45 +1,184 @@
 package com.petuser.petuser.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.petuser.petuser.api.request.UsuarioCreateRequest;
+import com.petuser.petuser.api.request.UsuarioLoginRequest;
+import com.petuser.petuser.api.request.UsuarioPatchRequest;
+import com.petuser.petuser.api.response.UsuarioGetResponse;
 import com.petuser.petuser.model.Usuario;
+import com.petuser.petuser.repository.UsuarioRepository;
 
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
-    private final List<Usuario> usuarios = new ArrayList<>();
 
-    public UsuarioServiceImpl(){
-        usuarios.add(new Usuario(1, "Sebastián", "Valdivia", "se.valdivia@duocuc.cl", "12345678", "asd #123", "Dueño de Mascota"));
-        usuarios.add(new Usuario(2, "Valentina", "Marchant", "valentina.marchant@gmail.com", "123456678", "asd #123","Conductor de Transporte Pet-Friendly"));
-        usuarios.add(new Usuario(3, "Andres", "Valdivia", "andres.valdivia@gmail.com", "123456789", "asd #123", "Dueño de Mascota"));
-    }
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @Override
-    public List<Usuario> getAllUsuarios() {
+    public List<UsuarioGetResponse> getAllUsuarios() {
         
-        return usuarios; 
+        return usuarioRepository.findAll().stream()
+                .map(usuario -> UsuarioGetResponse.builder()
+                        .id(usuario.getId())
+                        .nombre(usuario.getNombre())
+                        .apellido(usuario.getApellido())
+                        .email(usuario.getEmail())
+                        .telefono(usuario.getTelefono())
+                        .direccion(usuario.getDireccion())
+                        .rol(usuario.getRol())
+                        .build())
+                .toList();
     }
 
     @Override
-    public Usuario getUsuarioById(int id) {
-        Usuario usuario = usuarios.stream()
-                .filter(u -> u.getId() == id)
-                .findFirst()
-                .orElse(null);
+    public UsuarioGetResponse getUsuarioById(Long id) {
+
+        Usuario usuario = usuarioRepository.findById(id).orElse(null);
         if (usuario != null) {
-            return usuario;
-        } else {
-            return null;
+            return UsuarioGetResponse.builder()
+                    .id(usuario.getId())
+                    .nombre(usuario.getNombre())
+                    .apellido(usuario.getApellido())
+                    .email(usuario.getEmail())
+                    .telefono(usuario.getTelefono())
+                    .direccion(usuario.getDireccion())
+                    .rol(usuario.getRol())
+                    .build();
         }
+        return null;
     }
 
     @Override
-    public Usuario addUsuario (Usuario usuario) {
-        usuarios.add(usuario);
-        return usuario;
+    public UsuarioGetResponse addUsuario (UsuarioCreateRequest usuarioCreateRequest) {
+        Usuario usuario = Usuario.builder()
+                .nombre(usuarioCreateRequest.getNombre())
+                .apellido(usuarioCreateRequest.getApellido())
+                .email(usuarioCreateRequest.getEmail())
+                .password(usuarioCreateRequest.getPassword())
+                .telefono(usuarioCreateRequest.getTelefono())
+                .direccion(usuarioCreateRequest.getDireccion())
+                .rol(usuarioCreateRequest.getRol())
+                .build();
+        usuarioRepository.save(usuario);
+        return UsuarioGetResponse.builder()
+                .id(usuario.getId())
+                .nombre(usuario.getNombre())
+                .apellido(usuario.getApellido())
+                .email(usuario.getEmail())
+                .telefono(usuario.getTelefono())
+                .direccion(usuario.getDireccion())
+                .rol(usuario.getRol())
+                .build();
+    }
+
+    @Override
+    public UsuarioGetResponse login(UsuarioLoginRequest usuarioLoginRequest) {
+        Usuario usuario = usuarioRepository.findByEmail(usuarioLoginRequest.getEmail());
+        if (usuario != null && usuario.getPassword().equals(usuarioLoginRequest.getPassword())) {
+
+            UsuarioGetResponse usuarioLoginResponse = UsuarioGetResponse.builder()
+                    .id(usuario.getId())
+                    .nombre(usuario.getNombre())
+                    .apellido(usuario.getApellido())
+                    .email(usuario.getEmail())
+                    .telefono(usuario.getTelefono())
+                    .direccion(usuario.getDireccion())
+                    .rol(usuario.getRol())
+                    .build();
+            return usuarioLoginResponse;
+        }
+        return null;
+    }
+    @Override
+    public UsuarioGetResponse getUsuarioByEmail(String email) {
+        Usuario usuario = usuarioRepository.findByEmail(email);
+        if (usuario != null) {
+            return UsuarioGetResponse.builder()
+                    .id(usuario.getId())
+                    .nombre(usuario.getNombre())
+                    .apellido(usuario.getApellido())
+                    .email(usuario.getEmail())
+                    .telefono(usuario.getTelefono())
+                    .direccion(usuario.getDireccion())
+                    .rol(usuario.getRol())
+                    .build();
+        }
+        return null;
+    }
+    @Override
+    public boolean deleteUsuario(Long id) {
+        if (usuarioRepository.existsById(id)) {
+            usuarioRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public UsuarioGetResponse updateUsuario(Long id, UsuarioCreateRequest usuarioUpdateRequest) {
+        Usuario usuario = usuarioRepository.findById(id).orElse(null);
+        if (usuario != null) {
+            usuario.setNombre(usuarioUpdateRequest.getNombre());
+            usuario.setApellido(usuarioUpdateRequest.getApellido());
+            usuario.setEmail(usuarioUpdateRequest.getEmail());
+            usuario.setTelefono(usuarioUpdateRequest.getTelefono());
+            usuario.setDireccion(usuarioUpdateRequest.getDireccion());
+            usuario.setRol(usuarioUpdateRequest.getRol());
+            usuario.setPassword(usuarioUpdateRequest.getPassword());
+            usuarioRepository.save(usuario);
+            return UsuarioGetResponse.builder()
+                    .id(usuario.getId())
+                    .nombre(usuario.getNombre())
+                    .apellido(usuario.getApellido())
+                    .email(usuario.getEmail())
+                    .telefono(usuario.getTelefono())
+                    .direccion(usuario.getDireccion())
+                    .rol(usuario.getRol())
+                    .build();
+        }
+        return null;
+    }
+    @Override
+    public UsuarioGetResponse patchUsuario(Long id, UsuarioPatchRequest usuarioPatchRequest) {
+        Usuario usuario = usuarioRepository.findById(id).orElse(null);
+        if (usuario != null) {
+            if (usuarioPatchRequest.getNombre() != null) {
+                usuario.setNombre(usuarioPatchRequest.getNombre());
+            }
+            if (usuarioPatchRequest.getApellido() != null) {
+                usuario.setApellido(usuarioPatchRequest.getApellido());
+            }
+            if (usuarioPatchRequest.getEmail() != null) {
+                usuario.setEmail(usuarioPatchRequest.getEmail());
+            }
+            if (usuarioPatchRequest.getTelefono() != null) {
+                usuario.setTelefono(usuarioPatchRequest.getTelefono());
+            }
+            if (usuarioPatchRequest.getDireccion() != null) {
+                usuario.setDireccion(usuarioPatchRequest.getDireccion());
+            }
+            if (usuarioPatchRequest.getRol() != null) {
+                usuario.setRol(usuarioPatchRequest.getRol());
+            }
+            if (usuarioPatchRequest.getPassword() != null) {
+                usuario.setPassword(usuarioPatchRequest.getPassword());
+            }
+            usuarioRepository.save(usuario);
+            return UsuarioGetResponse.builder()
+                    .id(usuario.getId())
+                    .nombre(usuario.getNombre())
+                    .apellido(usuario.getApellido())
+                    .email(usuario.getEmail())
+                    .telefono(usuario.getTelefono())
+                    .direccion(usuario.getDireccion())
+                    .rol(usuario.getRol())
+                    .build();
+        }
+        return null;
     }
 
 }
