f5001016cbdb67f1f65bfbe9d966849f2ae50f26
valdo0 <sebastian.valdivia97gmail.com>
2025-04-20 21:42:01 -0400

    Agrega la clase UsuarioCreateRequest con validaciones para la creación de usuarios

---
+++ src/main/java/com/petuser/petuser/api/request/UsuarioCreateRequest.java
@@ -0,0 +1,36 @@
+package com.petuser.petuser.api.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UsuarioCreateRequest {
+    @NotNull
+    @Size(max=50)
+    private String nombre;
+
+    @Size(max=50)
+    private String apellido;
+
+    @Email
+    @NotNull
+    private String email;
+
+    @NotNull
+    @Size(min=8, max=20)
+    private String telefono;
+
+    @NotNull
+    private String direccion;
+
+    @NotNull
+    @Size(min=8, max=20)
+    private String rol;
+
+    @NotNull
+    private String password;
+}
