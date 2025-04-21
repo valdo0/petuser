eb0eb8de334a6ec0e082abb3f38a4c642e43a085
valdo0 <sebastian.valdivia97gmail.com>
2025-04-20 21:42:26 -0400

    Agrega métodos para la gestión de usuarios en UsuarioController, incluyendo creación, actualización, eliminación y login, con validaciones y manejo de errores.

--- src/main/java/com/petuser/petuser/controller/UsuarioController.java
+++ src/main/java/com/petuser/petuser/controller/UsuarioController.java
@@ -2,38 +2,133 @@ package com.petuser.petuser.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.petuser.petuser.model.Usuario;
+import com.petuser.petuser.api.request.UsuarioCreateRequest;
+import com.petuser.petuser.api.request.UsuarioLoginRequest;
+import com.petuser.petuser.api.request.UsuarioPatchRequest;
+import com.petuser.petuser.api.response.UsuarioGetResponse;
 import com.petuser.petuser.service.UsuarioService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
 @RestController
 @RequestMapping("/api/v1/usuarios")
 public class UsuarioController {
+    
+    private static final Logger logger = LoggerFactory.getLogger(UsuarioController.class);
     @Autowired
     private UsuarioService usuarioService;
 
     @GetMapping
-    public List<Usuario> getAllUsuarios() {
-        return usuarioService.getAllUsuarios();
+    public ResponseEntity<List<UsuarioGetResponse>> getAllUsuarios() {
+        logger.info("Getting all users: {}");
+        List<UsuarioGetResponse> usuarios = usuarioService.getAllUsuarios();
+        logger.info("Usuarios: {}", usuarios.size());
+        return new ResponseEntity<>(usuarios, HttpStatus.OK);
     }
     @PostMapping
-    public Usuario postMethodName(@RequestBody Usuario usuario) {
-        return usuarioService.addUsuario(usuario);
+    public ResponseEntity<UsuarioGetResponse> createUsuario(@RequestBody UsuarioCreateRequest usuario) {
+        logger.info("Creating user: {}", usuario);
+        if(usuarioService.getUsuarioByEmail(usuario.getEmail()) != null){
+            logger.error("User already exists: {}", usuario);
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        if(!usuario.getRol().equals("conductor de transporte pet-friendly") && !usuario.getRol().equals("dueño de mascota")){
+            logger.error("Invalid role: {}", usuario);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        UsuarioGetResponse usuarioResponse = usuarioService.addUsuario(usuario);
+        if (usuarioResponse == null) {
+            logger.error("Can't Create User: {}", usuario);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        logger.info("User created: {}", usuarioResponse);
+        return new ResponseEntity<>(usuarioResponse,HttpStatus.CREATED);
     }
     
-    @GetMapping("/{id}")
-    public Usuario getMethodName(@PathVariable int id) {
-        return usuarioService.getUsuarioById(id);
+    @GetMapping("/{id:\\d+}")
+    public ResponseEntity<UsuarioGetResponse> getUsuarioById(@PathVariable Long id) {
+        logger.info("Getting user by id: {}", id);
+        UsuarioGetResponse usuario = usuarioService.getUsuarioById(id);
+        if (usuario == null) {
+            logger.error("User not found: {}", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        logger.info("User found: {}", usuario);
+        return new ResponseEntity<>(usuario, HttpStatus.OK);
     }
-    
+    @PostMapping("/login")
+    public ResponseEntity<UsuarioGetResponse> login(@RequestBody UsuarioLoginRequest usuario) {
+        logger.info("Logging in user: {}", usuario);
+        UsuarioGetResponse usuarioResponse = usuarioService.login(usuario);
+        if (usuarioResponse == null) {
+            logger.error("User not found: {}", usuario);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        logger.info("User logged in: {}", usuarioResponse);
+
+        return new ResponseEntity<>(usuarioResponse,HttpStatus.OK);
+    }
+    @DeleteMapping("/{id:\\d+}")
+    public ResponseEntity<Void> deleteUsuario(@PathVariable Long id) {
+        logger.info("Deleting user: {}", id);
+        if (usuarioService.deleteUsuario(id)) {
+            logger.info("User deleted: {}", id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        logger.error("User not found: {}", id);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    @PutMapping("/{id:\\d+}")
+    public ResponseEntity<UsuarioGetResponse> updateUser(@PathVariable Long id, @RequestBody UsuarioCreateRequest usuario) {
+        logger.info("Updating user: {}", id);
+        if (usuarioService.getUsuarioById(id) == null) {
+            logger.error("User not found: {}", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        if(usuarioService.getUsuarioByEmail(usuario.getEmail()) != null){
+            logger.error("User already exists: {}", usuario);
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        if(!usuario.getRol().equals("conductor de transporte pet-friendly") && !usuario.getRol().equals("dueño de mascota")){
+            logger.error("Invalid role: {}", usuario);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        UsuarioGetResponse user = usuarioService.updateUsuario(id, usuario);
+        logger.info("User Updated: {}", user);
+        return new ResponseEntity<>(user,HttpStatus.OK);
+    }
+    @PatchMapping("/{id:\\d+}")
+    public ResponseEntity<UsuarioGetResponse> updateUsuario(@PathVariable Long id, @RequestBody UsuarioPatchRequest usuario) {
+        logger.info("Updating user: {}", id);
+        if(usuario.getRol()!= null){
+            if(!usuario.getRol().equals("conductor de transporte pet-friendly") && !usuario.getRol().equals("dueño de mascota")){
+                logger.error("Invalid role: {}", usuario);
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+        }
+        if (usuarioService.getUsuarioById(id) == null) {
+            logger.error("User not found: {}", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        UsuarioGetResponse usuarioResponse = usuarioService.patchUsuario(id, usuario);
+        logger.info("User updated: {}", usuarioResponse);
+        return new ResponseEntity<>(usuarioResponse,HttpStatus.OK);
+    }
 
 }
